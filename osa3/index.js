6a6e49f01c176f29c2cc00fea229ee92b96aed0b
iinanikkarikoski <iina.nikkarikoski@gmail.com>
2025-03-28 21:59:43 +0200

    osa3.11 done

--- osa3/index.js
+++ osa3/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express();
 app.use(express.json());
-app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('dist'));
