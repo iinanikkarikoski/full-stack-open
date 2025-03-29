b4c7556cea9fce497395ea9d6f597051b9562906
iinanikkarikoski <iina.nikkarikoski@gmail.com>
2025-03-29 21:23:42 +0200

    ..

--- osa3/index.js
+++ osa3/index.js
@@ -19,7 +19,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 //db
 ////////////////////////////////////////////////////////////////////////////////////////
-const url = MONGODB_URI;
+const url = process.env.MONGODB_URI;
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
