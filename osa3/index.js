6dac82587d4e4b3a1619ced4ba278d2ef47628ca
iinanikkarikoski <iina.nikkarikoski@gmail.com>
2025-03-29 22:58:56 +0200

    osa3

--- osa3/index.js
+++ osa3/index.js
@@ -1,30 +1,31 @@
-const express = require('express');
+require('dotenv').config()
+const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('dist'));
+  app.use(express.static('dist'))
 }
 
 morgan.token('body', (req) => {
   if(req.body) {
-    return JSON.stringify(req.body);
+    return JSON.stringify(req.body)
   }
-  return '';
-});
+  return ''
+})
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 //db
 ////////////////////////////////////////////////////////////////////////////////////////
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
-  .then(() => console.log("Connected to the database"))
-  .catch(error => console.log("Error connectin to the database: ", error));
+  .then(() => console.log('Connected to the database'))
+  .catch(error => console.log('Error connectin to the database: ', error))
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -46,70 +47,55 @@ personSchema.set('toJSON', {
 const Person = mongoose.model('Person', personSchema)
 ///////////////////////////////////////////////////////////////////////////////////////
 
-let persons = [
-  { id: 1, 
-    name: "Arto Hellas", 
-    number: "040-123456" },
-  { id: 2, 
-    name: "Ada Lovelace", 
-    number: "39-44-5323523" },
-  { id: 3, 
-    name: "Dan Abramov", 
-    number: "12-43-234345" },
-  { id: 4, 
-    name: "Mary Poppendieck", 
-    number: "39-23-6423122" }
-];
-
 app.get('/info', async (req, res) => {
-  const count = await Person.countDocuments();
-  const date = new Date();
+  const count = await Person.countDocuments()
+  const date = new Date()
   res.send(`
     <h1>Phonebook</h1>
     <p>Phonebook has info for ${count} people</p>
     <p>${date}</p>
   `)
-});
+})
 
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(persons => {
-    res.json(persons);
+    res.json(persons)
   })
-});
+})
 
 app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
-    .then(person=> {
+    .then(person => {
       if (person) {
-        res.json(person);
+        res.json(person)
       } else {
-        res.status(404).send({error: "Person not found"});
+        res.status(404).send({ error: 'Person not found' })
       }
     })
-    .catch(error => next(error)); 
-});
+    .catch(error => next(error))
+})
 
 app.post('/api/persons', async (req, res, next) => {
-  const {name, number} = req.body;
+  const { name, number } = req.body
 
   if (!name || !number) {
-    return res.status(400).json({error: "Name or number missing"});
+    return res.status(400).json({ error: 'Name or number missing' })
   }
 
-  const existingPerson = await Person.findOne({name});
+  const existingPerson = await Person.findOne({ name })
   if (existingPerson) {
-    return res.status(400).json({error: "Name must be unique"});
+    return res.status(400).json({ error: 'Name must be unique' })
   }
 
-  const person = new Person({ name, number });
+  const person = new Person({ name, number })
 
   person.save()
     .then(savedPerson => res.status(201).json(savedPerson))
     .catch(error => next(error))
-});
+})
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const {name, number} = req.body
+  const { name, number } = req.body
 
   Person.findById(req.params.id)
     .then(person => {
@@ -129,26 +115,26 @@ app.put('/api/persons/:id', (req, res, next) => {
 
 app.delete('/api/persons/:id', async (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.status(400).json({ error: "Invalid ID format" });
+    return res.status(400).json({ error: 'Invalid ID format' })
   }
 
   try {
-    const person = await Person.findByIdAndDelete(req.params.id);
+    const person = await Person.findByIdAndDelete(req.params.id)
     if (!person) {
-      return res.status(404).json({ error: "Person not found" });
+      return res.status(404).json({ error: 'Person not found' })
     }
-    res.status(204).end();
+    res.status(204).end()
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
+  console.error(error.message)
   if (error.name === 'CastError') {
-    return res.status(400).json({ error: 'Malformatted ID' });
+    return res.status(400).json({ error: 'Malformatted ID' })
   } else if (error.name === 'ValidationError') {
-    return res.status(400).json({error: error.message})
+    return res.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -156,7 +142,7 @@ const errorHandler = (error, req, res, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
